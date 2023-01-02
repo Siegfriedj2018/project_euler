a7a13c1a0925cb14fec78889129484a7067216c4
Siegfriedj2020 <justasiegfried09@gmail.com>
2023-01-02 11:24:03 -0500

    added problem 10

---
+++ problem10.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using std::cout;
+using std::endl;
+
+int main() {
+    int SumPrimes = 17;
+    int maxValue = 2000000;
+    for (size_t i = 7; i <= maxValue; i += 1) {
+        for (size_t j = 0; j <= sqrt(i); ++j) {
+            if (fmod(i, j) == 0) {
+                
+            }
+        }
+    }
+    // 104743
+    
+    // cout << __cplusplus << endl;
+}
